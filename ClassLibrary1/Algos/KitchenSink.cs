e4dafae7c42bb1c48a4963f00cfbfede7bd2ce1e
andreacremese <cremese@gmail.com>
2017-03-15 16:51:09 -0700

    added rot exercise

---
+++ ClassLibrary1/Algos/KitchenSink.cs
@@ -0,0 +1,19 @@
+﻿// a mixin of various algos and samples
+
+using System;
+
+namespace ClassLibrary1.Algos {
+    public class KitchenSink {
+        // rotates a string unitl the first character is 'a'.
+        public static string RotA(String s) {
+            if (s == String.Empty) { return s; }
+            if (s.Length == 1) { return "a"; }
+            var steps = Convert.ToInt32('z') - Convert.ToInt32(s[0]) + 1;
+            var result = String.Empty;
+            for (var i = 0; i < s.Length; i++) {
+                result = result + Convert.ToChar((Convert.ToInt32(s[i]) + steps - Convert.ToInt32('a')) % 26 + Convert.ToInt32('a'));
+            }
+            return result;
+        }
+    }
+}
---
+++ Tests/KitchenSink.Test.cs
@@ -0,0 +1,52 @@
+﻿using ClassLibrary1.Algos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class KitchenSinkTest {
+        [TestMethod]
+        public void CanRotAnEmptyString() {
+            // arrange
+            // act
+            var output = KitchenSink.RotA("");
+            // assert
+            Assert.AreEqual("", output);
+        }
+
+        [TestMethod]
+        public void CanRotAOneCharacterString() {
+            // arrange
+            // act
+            var output = KitchenSink.RotA("z");
+            // assert
+            Assert.AreEqual("a", output);
+        }
+
+        [TestMethod]
+        public void CanRotAOneCharacterStringThatIsA() {
+            // arrange
+            // act
+            var output = KitchenSink.RotA("a");
+            // assert
+            Assert.AreEqual("a", output);
+        }
+
+        [TestMethod]
+        public void CanRotAStringWithNoWrapping() {
+            // arrange
+            // act
+            var output = KitchenSink.RotA("bcd");
+            // assert
+            Assert.AreEqual("abc", output);
+        }
+
+        [TestMethod]
+        public void CanRotAStringWithWrapping() {
+            // arrange
+            // act
+            var output = KitchenSink.RotA("zab");
+            // assert
+            Assert.AreEqual("abc", output);
+        }
+    }
+}
