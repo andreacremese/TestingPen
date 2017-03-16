e336d41b860d813e4796fff0ebcd7dac09dae362
andreacremese <cremese@gmail.com>
2017-03-15 18:07:59 -0700

    added some algos

--- ClassLibrary1/Algos/KitchenSink.cs
+++ ClassLibrary1/Algos/KitchenSink.cs
@@ -1,6 +1,7 @@
-﻿// a mixin of various algos and samples
+﻿// a mixin of various algos and samples found across various sources
 
 using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary1.Algos {
     public class KitchenSink {
@@ -13,6 +14,48 @@ namespace ClassLibrary1.Algos {
             for (var i = 0; i < s.Length; i++) {
                 result = result + Convert.ToChar((Convert.ToInt32(s[i]) + steps - Convert.ToInt32('a')) % 26 + Convert.ToInt32('a'));
             }
+            return result;
+        }
+
+
+        // return the maximum sum of not consecutive numbers in an array
+        public static Int32 FindMax(Int32[] arr) {
+            if (arr == null) { throw new Exception("Sledge hammer"); }
+            // could do in backtracking, but that is n!
+            // so, inspired by dynamic programming
+            // keep in "memor" alreay computed results for i-s of arr
+            // the key is the cursor in the array, the value is the maximum sum beyond that index
+
+            var intermediate = new Dictionary<Int32, Int32>();
+            return FindMax(arr, 0, intermediate);
+        }
+
+        private static Int32 FindMax(Int32[] arr, Int32 i, Dictionary<Int32, Int32> intermediate) {
+            // no null checks as I know what is coming in
+            // this is the part where we use sort of dynamic programming
+            if (intermediate.ContainsKey(i)) { return intermediate[i]; }
+            Int32 result;
+            if (i + 2 == arr.Length) {
+                // in this case I have only the two last elements left
+                // so the result is the maximum of the last two elements
+                result = Math.Max(arr[arr.Length - 1], arr[arr.Length - 2]);
+                intermediate[i] = result;
+                return result;
+            }
+            if (i + 1 == arr.Length) {
+                // in this case I have only the last elements left
+                // so the result is the maximum of the last two elements
+                result = arr[arr.Length - 1];
+                intermediate[i] = result;
+                return result;
+            }
+
+            // for sure i < lenght -2, I have 3 elements left
+            // at this point I can either select i or not
+            var selectI = arr[i] + FindMax(arr, i + 2, intermediate);
+            var notSelectI = FindMax(arr, i + 1, intermediate);
+            result = Math.Max(selectI, notSelectI);
+            intermediate[i] = result;
             return result;
         }
     }
--- Tests/KitchenSink.Test.cs
+++ Tests/KitchenSink.Test.cs
@@ -1,11 +1,12 @@
 ﻿using ClassLibrary1.Algos;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Tests {
     [TestClass]
     public class KitchenSinkTest {
         [TestMethod]
-        public void CanRotAnEmptyString() {
+        public void RotAnEmptyString() {
             // arrange
             // act
             var output = KitchenSink.RotA("");
@@ -14,7 +15,7 @@ namespace Tests {
         }
 
         [TestMethod]
-        public void CanRotAOneCharacterString() {
+        public void RotAOneCharacterString() {
             // arrange
             // act
             var output = KitchenSink.RotA("z");
@@ -23,7 +24,7 @@ namespace Tests {
         }
 
         [TestMethod]
-        public void CanRotAOneCharacterStringThatIsA() {
+        public void RotAOneCharacterStringThatIsA() {
             // arrange
             // act
             var output = KitchenSink.RotA("a");
@@ -32,7 +33,7 @@ namespace Tests {
         }
 
         [TestMethod]
-        public void CanRotAStringWithNoWrapping() {
+        public void RotAStringWithNoWrapping() {
             // arrange
             // act
             var output = KitchenSink.RotA("bcd");
@@ -41,12 +42,62 @@ namespace Tests {
         }
 
         [TestMethod]
-        public void CanRotAStringWithWrapping() {
+        public void RotAStringWithWrapping() {
             // arrange
             // act
             var output = KitchenSink.RotA("zab");
             // assert
             Assert.AreEqual("abc", output);
         }
+
+        // ================================================
+
+        [TestMethod]
+        public void FindMaxArrayOntwoLengthArray() {
+            // act
+            var output = KitchenSink.FindMax(new Int32[2] { 1, 2 });
+            // assert
+            Assert.AreEqual(2, output);
+        }
+
+        [TestMethod]
+        public void FindMaxArrayOnOneLengthArray() {
+            // act
+            var output = KitchenSink.FindMax(new Int32[1] { 2 });
+            // assert
+            Assert.AreEqual(2, output);
+        }
+
+        [TestMethod]
+        public void FindMaxArrayOnThreeLengthArray() {
+            // act
+            var output = KitchenSink.FindMax(new Int32[3] { 1, 2, 3 });
+            // assert
+            Assert.AreEqual(4, output);
+        }
+
+        [TestMethod]
+        public void FindMaxArrayOnSixLengthArray() {
+            // act
+            var output = KitchenSink.FindMax(new Int32[6] { 1, 8, 1, 1, 8 ,1 });
+            // assert
+            Assert.AreEqual(16, output);
+        }
+
+        [TestMethod]
+        public void FindMaxArray24LengthArray() {
+            // arrange
+            var inputPart = new Int32[6] { 1, 8, 1, 1, 8, 1 };
+            var input = new Int32[6 * 10];
+            for (var i = 0; i < 10; i++) {
+                inputPart.CopyTo(input, 6*i);
+            }
+            // act
+            var output = KitchenSink.FindMax(input);
+            // assert
+            Assert.AreEqual(160, output);
+        }
+
+
     }
 }
