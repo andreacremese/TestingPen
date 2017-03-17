10eb733fc526ba16e41ebb0e37b1ebff500369a4
andreacremese <cremese@gmail.com>
2017-03-16 17:47:35 -0700

    trie added

---
+++ ClassLibrary1/Structures/MyTrie.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Structures {
+    public class MyTrie {
+        public Node root;
+        private Dictionary<String,Int32> counter;
+
+        public MyTrie() {
+            root = new Node('z');
+            counter = new Dictionary<string, Int32>();
+        }
+
+        public void Add(String s) {
+            AddInNode(s, 0,root);
+        }
+
+        public Int32 FindTraversing(String s) {
+            return FindInNode(s, 0, root);
+        }
+
+        public Int32 Find(String s) {
+            if (counter.ContainsKey(s)) {
+                return counter[s];
+            } else {
+                return 0;
+            }
+        }
+
+        private void AddInNode(String s, Int32 i, Node parent) {
+            // null check
+            if (parent == null) { throw new Exception("..."); }
+            // if i == string.Lenght
+            // parent.isComplete = true, and return
+            if (i == s.Length) {
+                parent.isComplete = true;
+                return;
+            }
+            if (counter.ContainsKey(s.Substring(0, i + 1))) {
+                counter[s.Substring(0, i + 1)]++;
+            } else {
+                counter[s.Substring(0, i + 1)] = 1;
+            }
+            // find if s[i] exists in parent.next
+            Node node;
+            if (parent.next.ContainsKey(s[i])) {
+                node = parent.next[s[i]];
+            } else {
+                node = new Node(s[i]);
+                parent.next[s[i]] = node;
+            }
+            AddInNode(s, i + 1, node);
+            return;
+            // findInNode from that node for i +1
+        }
+
+        private Int32 FindInNode(String s, Int32 i, Node parent) {
+            // null check
+            if (parent == null) { throw new Exception("..."); }
+            if (i == s.Length) {
+                return FindNumberOfWords(parent);
+            }
+            if (parent.next.ContainsKey(s[i])) {
+                return FindInNode(s, i + 1, parent.next[s[i]]);
+            } else {
+                return 0;
+            }
+        }
+
+        private Int32 FindNumberOfWords(Node n) {
+            var result = 0;
+            if (n.isComplete) {
+                result++;
+            }
+            foreach (var i in n.next) {
+                result = result + FindNumberOfWords(i.Value);
+            }
+            return result;
+        }
+
+
+        public class Node {
+            public Char letter;
+            public Dictionary<Char,Node> next;
+            public Boolean isComplete;
+
+            public Node(Char _l) {
+                letter = _l;
+                next = new Dictionary<Char, Node>();
+                isComplete = false;
+            }
+        }
+    }
+}
---
+++ Tests/MyTrie.Test.cs
@@ -0,0 +1,70 @@
+﻿using ClassLibrary1.Structures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class MyTrieTest {
+        [TestMethod]
+        public void TrieCanAddWord() {
+            //  arrange
+            var _sut = new MyTrie();
+            // act
+            _sut.Add("ab");
+            _sut.Add("ac");
+            _sut.Add("abc");
+            // assert
+            // first
+            Assert.AreEqual(1, _sut.root.next.Count);
+            var first = _sut.root.next['a'];
+            Assert.AreEqual('a', first.letter);
+            Assert.IsFalse(first.isComplete);
+            Assert.AreEqual(2, first.next.Count);
+            // second level
+            var second = first.next['c'];
+            Assert.IsNotNull(second);
+            Assert.AreEqual(0, second.next.Count);
+            Assert.IsTrue(second.isComplete);
+            second = first.next['b'];
+            Assert.IsNotNull(second);
+            Assert.AreEqual(1, second.next.Count);
+            Assert.IsTrue(second.isComplete);
+        }
+
+        [TestMethod]
+        public void TrieCanFindWordCount() {
+            //  arrange
+            var _sut = new MyTrie();
+            // using a method in arrange is not a good practice
+            // refactor and generate nodes manually.
+            _sut.Add("ab");
+            _sut.Add("ac");
+            _sut.Add("abc");
+            // act
+            var output1 = _sut.Find("a");
+            var output2 = _sut.Find("ab");
+            var output3 = _sut.Find("z");
+            // assert
+            // first
+            Assert.AreEqual(3, output1);
+            Assert.AreEqual(2, output2);
+            Assert.AreEqual(0, output3);
+        }
+
+
+        //[TestMethod]
+        //public void ProfileTrie() {
+        //    //  arrange
+        //    var _sut = new MyTrie();
+        //    // using a method in arrange is not a good practice
+        //    // refactor and generate nodes manually.
+        //    _sut.Add("ab");
+        //    _sut.Add("ac");
+        //    _sut.Add("abc");
+        //    // act
+        //    var output1 = _sut.Find("a");
+        //    var output2 = _sut.Find("ab");
+        //    var output3 = _sut.Find("z");
+            
+        //}
+    }
+}
