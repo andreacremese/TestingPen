4478097b88ca858152a5ba952bc0cf11b93b5399
v-ancrem <v-ancrem@microsoft.com>
2017-03-22 18:01:16 -0700

    dotnet core first commit

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Alogs and data structures in the `Library` project, Tests in the `Tests` project
 Run all tests in Visual studio (ctrl T + A). Note that some are expected to throw (it is part of the test...).
 
 Form Leetcode.com, Cracking the Code Interview, Steve Skienna's CSE 373 and other sources. 
+
+
+
+# Running in OSX/ Linux
+
+The project can be build and run in linux as well.
+
+## install .net core
+Install dotnet core from [here](https://www.microsoft.com/net/core#macos).
+
+
+
+Some resources https://docs.microsoft.com/en-us/dotnet/articles/core/tutorials/libraries#how-to-use-multiple-projects
--- Tests/HackerRankBFS.cs
+++ Tests/HackerRankBFS.cs
@@ -55,55 +55,55 @@ namespace Tests {
             CollectionAssert.AreEqual(expected, result);
         }
 
-        [Test]
-        public void TestMethod3() {
-            // arrange
-            String line;
-            // Read the file and display it line by line.
-            var fullPath = Directory.GetCurrentDirectory();
-            var path = Path.GetFullPath(Path.Combine(fullPath, "Tests"));
-            System.IO.StreamReader file = new System.IO.StreamReader(Path.Combine(path,"BFSinput.txt"));
-            while ((line = file.ReadLine()) != null) {
-                // n of queries
-                var q = Convert.ToInt32(line);
-                var result = new List<Int32>[q];
+        // [Test]
+        // public void TestMethod3() {
+        //     // arrange
+        //     String line;
+        //     // Read the file and display it line by line.
+        //     var fullPath = Directory.GetCurrentDirectory();
+        //     var path = Path.GetFullPath(Path.Combine(fullPath, "Tests"));
+        //     System.IO.StreamReader file = new System.IO.StreamReader(Path.Combine(path,"BFSinput.txt"));
+        //     while ((line = file.ReadLine()) != null) {
+        //         // n of queries
+        //         var q = Convert.ToInt32(line);
+        //         var result = new List<Int32>[q];
 
 
 
-                for (int j = 0; j < q; j++) {
-                    var l1 = file.ReadLine();
-                    var n = Convert.ToInt32(l1.Split(' ')[0]);
-                    var m = Convert.ToInt32(l1.Split(' ')[1]);
+        //         for (int j = 0; j < q; j++) {
+        //             var l1 = file.ReadLine();
+        //             var n = Convert.ToInt32(l1.Split(' ')[0]);
+        //             var m = Convert.ToInt32(l1.Split(' ')[1]);
 
-                    // create graph
-                    var graph = new Graph();
-                    // add vertices to graph
-                    for (int i = 1; i <= 1000; i++) {
-                        graph.AddVertice(i);
-                    }
-                    // add edges to graph
-                    for (int i = 0; i < m; i++) {
-                        l1 = file.ReadLine();
-                        graph.AddEdge(
-                            Convert.ToInt32(l1.Split(' ')[0]),
-                            Convert.ToInt32(l1.Split(' ')[1]));
-                    }
-                    // get s
-                    l1 = file.ReadLine();
-                    var s = Convert.ToInt32(l1);
-                    var localrResult = new List<Int32>();
-                    for (int i = 1; i <= n; i++) {
-                        if (i == s) { continue; }
-                        var r = graph.GetNumberOfSteps(s, i);
-                        localrResult.Add(r == -1 ? -1 : r * 6);
-                    }
-                    result[j] = localrResult;
-                }
+        //             // create graph
+        //             var graph = new Graph();
+        //             // add vertices to graph
+        //             for (int i = 1; i <= 1000; i++) {
+        //                 graph.AddVertice(i);
+        //             }
+        //             // add edges to graph
+        //             for (int i = 0; i < m; i++) {
+        //                 l1 = file.ReadLine();
+        //                 graph.AddEdge(
+        //                     Convert.ToInt32(l1.Split(' ')[0]),
+        //                     Convert.ToInt32(l1.Split(' ')[1]));
+        //             }
+        //             // get s
+        //             l1 = file.ReadLine();
+        //             var s = Convert.ToInt32(l1);
+        //             var localrResult = new List<Int32>();
+        //             for (int i = 1; i <= n; i++) {
+        //                 if (i == s) { continue; }
+        //                 var r = graph.GetNumberOfSteps(s, i);
+        //                 localrResult.Add(r == -1 ? -1 : r * 6);
+        //             }
+        //             result[j] = localrResult;
+        //         }
 
 
-            }
-            // make sure it can read the test input
-            Assert.AreEqual(1, 1);
-        }
+        //     }
+        //     // make sure it can read the test input
+        //     Assert.AreEqual(1, 1);
+        // }
     }
 }
