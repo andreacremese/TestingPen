b8b1eb13e8b20d69ee77abefee4dbe9d00431bf2
andreacremese <cremese@gmail.com>
2017-03-22 17:14:18 -0700

    added NUnit for mono testing

--- Tests/BinSearchTest.cs
+++ Tests/BinSearchTest.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using System;
 using ClassLibrary1.Leet;
 
 namespace Tests {
-    [TestClass]
+    [TestFixture]
     public class BinSearchTest {
-        [TestMethod]
+        [Test]
 
         public void BinSearch_can_Find_element() {
             var a = new Int32[6] { 0, 1, 2, 3, 4, 5 };
@@ -15,21 +15,21 @@ namespace Tests {
             }
         }
 
-        [TestMethod]
+        [Test]
         public void BinSearch_can_Find_element1() {
             var a = new Int32[6] { 0, 1, 2, 3, 4, 5 };
             var output = BinSearch.BinSearchInArr(a, 8);
             Assert.IsFalse(output);
         }
 
-        [TestMethod]
+        [Test]
         public void BinSearch_can_Find_element2() {
             var a = new Int32[2] { 0, 0 };
             var output = BinSearch.BinSearchInArr(a, 8);
             Assert.IsFalse(output);
         }
 
-        [TestMethod]
+        [Test]
         public void BinSearch_can_Find_element3() {
             var a = new Int32[5] { 1,1,2,3,4 };
             for (var i = 1; i < 5; i++) {
